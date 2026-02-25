b4f0d2f358271d3f37c6a4ef732e42c8f2702cb0
karimooos <errakarim@gmail.com>
2026-02-25 09:30:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import os
 import requests
 from bs4 import BeautifulSoup
-import time
 
-TOKEN = "8763454421:AAEA7zUmLVkXLBE6Hqty6RhdGwC9YvB2LcA"
-CHAT_ID = "1664966725"
-SEARCH_URL = "https://www.avito.ma/fr/maroc/voitures_d_occasion-%C3%A0_vendre?brand=49&model=megane&brand_model=49_megane&price=125000-145000&fuel=1&regdate=2017-2022&seller_type=0&has_price=true"
-
-sent_links = set()
+TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
+SEARCH_URL = os.environ.get("SEARCH_URL")
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
@@ -16,31 +14,22 @@ def send_telegram(message):
     }
     requests.post(url, data=data)
 
-while True:
-    try:
-        headers = {"User-Agent": "Mozilla/5.0"}
-        response = requests.get(SEARCH_URL, headers=headers)
-        soup = BeautifulSoup(response.text, "html.parser")
+headers = {"User-Agent": "Mozilla/5.0"}
+response = requests.get(SEARCH_URL, headers=headers)
+soup = BeautifulSoup(response.text, "html.parser")
 
-        ads = soup.find_all("a", href=True)
+ads = soup.find_all("a", href=True)
 
-        for ad in ads:
-            link = ad["href"]
+for ad in ads:
+    link = ad["href"]
 
-            if "/voitures_d_occasion/" in link:
-                full_link = "https://www.avito.ma" + link
+    if "/voitures_d_occasion/" in link:
+        full_link = "https://www.avito.ma" + link
+        title = ad.get_text(strip=True)
 
-                if full_link not in sent_links:
-                    sent_links.add(full_link)
+        parent = ad.find_parent()
+        text_block = parent.get_text(" ", strip=True)
 
-                    title = ad.get_text(strip=True)
-
-                    if "Megane" in title:
-                        message = f"🚗 Nouvelle annonce:\n{title}\n{full_link}"
-                        send_telegram(message)
-
-        time.sleep(300)
-
-    except Exception as e:
-        print("Error:", e)
-        time.sleep(300)
+        # نتحقق واش فيه minutes
+        if "minute" in text_block.lower():
+            send_telegram(f"🚗 Nouvelle annonce:\n{title}\n{full_link}")
